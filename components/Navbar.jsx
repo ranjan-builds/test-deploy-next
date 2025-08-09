df71048625b8b7862ab7a996afd3511990e1a469
ranjan-builds <ranjankashyap6475@gmail.com>
2025-08-09 21:42:37 +0530

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,25 @@
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <div className=" fixed flex items-center gap-3 left-1/2 -translate-x-1/2  justify-center py-3 ">
-      <div className=" border-2 border-gray-200 rounded-full py-2 px-4 flex gap-2 text-black">
-        <Link href="/" className="py-1 px-2 hover:text-blue-400">
-          Home
-        </Link>
-        <Link className="py-1 px-2 hover:text-blue-400" href="/About">
+    <div className="fixed flex items-center gap-3 left-1/2 -translate-x-1/2 justify-center py-3">
+      <div className="bg-slate-100/10 rounded-full py-2 px-4 flex gap-2">
+        {router.pathname === "/" ? (
+          <span className="py-1 px-2 text-blue-400">Home</span>
+        ) : (
+          <Link href="/" className="py-1 px-2 hover:text-blue-400">
+            Home
+          </Link>
+        )}
+
+        <Link href="/About" className="py-1 px-2 hover:text-blue-400">
           About
         </Link>
-
-        <Link className="py-1 px-2 hover:text-blue-400" href="/Work">
+        <Link href="/Work" className="py-1 px-2 hover:text-blue-400">
           Work
         </Link>
       </div>
