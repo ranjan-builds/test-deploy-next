a7592aebd75d1cf2380155919c090037b11e32f7
ranjan-builds <ranjankashyap6475@gmail.com>
2025-08-09 20:57:20 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // replaces `next export`
+  basePath: isProd ? '/test-deploy-next' : '',
+  assetPrefix: isProd ? '/test-deploy-next/' : '',
+  images: {
+    unoptimized: true, // required for static export
+  },
+};
 
 export default nextConfig;
