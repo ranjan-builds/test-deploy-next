9db36984a5293f7f6adeb9ed00ed711627f720aa
ranjan-builds <ranjankashyap6475@gmail.com>
2025-08-09 21:48:01 +0530

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,16 +7,24 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const pathname = usePathname();
 
+  const handleHomeClick = (e) => {
+    if (pathname === "/") {
+      e.preventDefault(); // stop browser reload
+    }
+  };
+
   return (
     <div className="fixed flex items-center gap-3 left-1/2 -translate-x-1/2 justify-center py-3">
       <div className="bg-slate-100/10 rounded-full py-2 px-4 flex gap-2">
-        {pathname === "/" ? (
-          <span className="py-1 px-2 text-blue-400">Home</span>
-        ) : (
-          <Link href="/" className="py-1 px-2 hover:text-blue-400">
-            Home
-          </Link>
-        )}
+        <Link
+          href="/"
+          className={`py-1 px-2 ${
+            pathname === "/" ? "text-blue-400" : "hover:text-blue-400"
+          }`}
+          onClick={handleHomeClick}
+        >
+          Home
+        </Link>
 
         <Link href="/About" className="py-1 px-2 hover:text-blue-400">
           About
