29dabda86e61b6f8ac20660c7bf5164b5c8700a5
ranjan-builds <ranjankashyap6475@gmail.com>
2025-08-09 21:44:21 +0530

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,16 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <div className="fixed flex items-center gap-3 left-1/2 -translate-x-1/2 justify-center py-3">
       <div className="bg-slate-100/10 rounded-full py-2 px-4 flex gap-2">
-        {router.pathname === "/" ? (
+        {pathname === "/" ? (
           <span className="py-1 px-2 text-blue-400">Home</span>
         ) : (
           <Link href="/" className="py-1 px-2 hover:text-blue-400">
